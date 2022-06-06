4e01c88126d1d241bfe5a1149bc3be23a499ede1
Diego Eiras <eiras.lucio@gmail.com>
2022-06-06 14:12:57 -0300

    📦 FEAT: Download video from YouTube

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,17 @@ import 'dotenv/config'
 
 import readline from 'readline-sync'
 import { google } from 'googleapis'
+import youtubedl from 'youtube-dl-exec'
+import path from 'path'
 
 async function main() {
-  const music = readline.question('What is the name of the music? ')
-  const artist = readline.question('What is the name of the artist? ')
+  const music = {
+    name: readline.question('What is the name of the artist? '),
+    artist: readline.question('What is the name of the music? '),
+    filename: '',
+    url: '',
+    duration: 0,
+  }
 
   const youtube = google.youtube({
     version: 'v3',
@@ -15,12 +22,28 @@ async function main() {
   const { data: { items: searchResult }} = await youtube.search.list({
     maxResults: 1,
     part: ['snippet'],
-    q: `${music} ${artist}`
+    q: `${music.artist} ${music.name}`
   })
 
   const videoId = searchResult && searchResult[0].id?.videoId
 
-  console.log(`https://www.youtube.com/watch?v=${videoId}`)
+  music.url = `https://www.youtube.com/watch?v=${videoId}` || ''
+  music.filename = `music_${videoId}`
+
+  await youtubedl(music.url, {
+    abortOnError: true,
+    output: path.resolve(
+      __dirname, '..', 'tmp', 'videos', `${music.filename}.%(ext)s`),
+  })
+
+  const result: unknown = await youtubedl(music.url, {
+    getDuration: true,
+  })
+
+  const duration = result as string
+
+  const [minutes, seconds] = duration.split(':').map(Number)
+  music.duration = minutes * 60 + seconds
 }
 
 main()
